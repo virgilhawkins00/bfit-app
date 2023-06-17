8ecc4a7d66f1fd148ba2812495a9a4227f43d040
Alex Muniz <munizalex45@gmail.com>
2023-06-17 15:51:45 -0300

    bfit-app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from "@react-navigation/native"
+import { DefaultTheme, NavigationContainer, Theme } from "@react-navigation/native"
 import RootNavigator from './src/navigators/RootNavigator';
+import { useMemo } from 'react';
 
 export default function App() {
+  const theme: Theme = useMemo(() => ({...DefaultTheme, colors: {...DefaultTheme.colors, background: "#f5f5f5", text: "#7168D3", border: "#D9D9D9", primary: "#7168D3" }}), [])
   return (
     <View style={styles.container}>
-      <NavigationContainer>
+      <NavigationContainer theme={theme}>
         <RootNavigator />
         <StatusBar style="dark" />
       </NavigationContainer>
