ba08e1a64e379f7f050338fe785a86ac1c944472
Alex Muniz <munizalex45@gmail.com>
2023-06-23 17:34:36 -0300

    update rootNavigator

--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -1,27 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { NavigatorScreenParams } from "@react-navigation/native"
-import {createNativeStackNavigator} from "@react-navigation/native-stack"
-import DetailsScreen from '../screens/DetailsScreen';
-import TabsNavigator, { TabsStackParamList } from './TabsNavigator';
+import React from "react";
+import { NavigatorScreenParams } from "@react-navigation/native";
+import {
+  NativeStackScreenProps,
+  createNativeStackNavigator,
+} from "@react-navigation/native-stack";
+import DetailsScreen from "../screens/DetailsScreen";
+import TabsNavigator, { TabsStackParamList } from "./TabsNavigator";
 
 export type RootStackParamList = {
-    TabsStack: NavigatorScreenParams<TabsStackParamList>;
-    DetailsScreen: undefined;
-}
+  TabsStack: NavigatorScreenParams<TabsStackParamList>;
+  Details: {
+    id: string;
+  };
+};
 
-const RootStack = createNativeStackNavigator<RootStackParamList> ()
+const RootStack = createNativeStackNavigator<RootStackParamList>();
+
+export type RootStackScreenProps<T extends keyof RootStackParamList> =
+  NativeStackScreenProps<RootStackParamList, T>;
 
 const RootNavigator = () => {
   return (
     <RootStack.Navigator>
-      <RootStack.Screen name="TabsStack" component={TabsNavigator} options={{
-        headerShown: false,
-      }}
+      <RootStack.Screen
+        name="TabsStack"
+        component={TabsNavigator}
+        options={{
+          headerShown: false,
+        }}
       />
-      <RootStack.Screen name="DetailsScreen" component={DetailsScreen}/>
+      <RootStack.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
     </RootStack.Navigator>
-  )
-}
+  );
+};
 
-export default RootNavigator
+export default RootNavigator;
--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,12 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import {RootStackScreenProps} from "../navigators/RootNavigator";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useTheme } from "@react-navigation/native";
+import Icons from "@expo/vector-icons/MaterialIcons"
 
-const DetailsScreen = () => {
-  return (
-    <View>
-      <Text>DetailsScreen</Text>
-    </View>
-  )
-}
+const DetailsScreen = ({
+  navigation,
+  route: {
+    params: {id},
+  },
+  }: RootStackScreenProps<"Details">) => {
+    const {colors} = useTheme();
+    return (
+      <View style={{ flex: 1}}>
+        <SafeAreaView edges={["top"]}>
+          <View style={{ flexDirection: "row", alignItems: "center"}}>
+            <TouchableOpacity
+                style={{
+                  width: 52,
+                  aspectRatio: 1,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 52,
+                  borderWidth: 1,
+                  borderColor: colors.border,
+                }}>
+                  <Icons name="notifications" size={24} color={colors.text} />
+                </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+        <Text>DetailsScreen</Text>
+      </View>
+    )
+  }
 
 export default DetailsScreen
